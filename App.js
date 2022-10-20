5d22a7b4ee687e33c3526ead7ee65a890c7de638
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-10-20 22:48:04 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text className="bg-red-500 text-2xl" >Fooddas</Text>
+      </View>
+    )
+  }
+}
+
+export default App
