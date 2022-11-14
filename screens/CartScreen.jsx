1f3c17caeadc0c2191bb6c559aa9ce2aa6acff26
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-14 23:29:38 +0000

    cart screen grouping items

--- screens/CartScreen.jsx
+++ screens/CartScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux';
 import { selectVendor } from '../features/VendorSlice';
@@ -9,13 +9,15 @@ const CartScreen = () => {
     const navigation = useNavigation();
     const vendor = useSelector(selectVendor)
     const items = useSelector(selectCartItems)
+    const [groupedItems, setGroupedItemsInCArt] = useState([]);
     const dispatch = useDispatch();
 
 
     useMemo(() => {
-        const groupedItems = items.reduce(() => {
+        const groupedItems = items.reduce((results, item) => {
             (results[item.id] = results[item.id] || []).push(item)
         })
+        setGroupedItemsInCArt(groupedItems);
     }, [items])
   return (
     <View className="">
