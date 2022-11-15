a2957a3b208993b2f4de7dfde576029236efd6b5
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-15 23:50:12 +0000

    payment methods lsitings

--- screens/PayMethodScreen.jsx
+++ screens/PayMethodScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, Image,ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -16,20 +16,55 @@ const PayMethodScreen = () => {
 
   return (
     <SafeAreaView>
-      <View>
-        <View className="flex-row m-3 justify-between items-center ">
+      <View className="bg-white">
+        <View className="flex-row m-3  items-center ">
           <TouchableOpacity onPress={navigation.goBack}>
             <Icon name="arrow-back" size={33} color="red" />
           </TouchableOpacity>
 
-          <Text className="text-lg font-bold text-black">
+          <Text className="text-xl m-1 font-bold text-black">
             Select Payment Method
           </Text>
         </View>
       </View>
 
       <ScrollView>
-        
+        <TouchableOpacity className="">
+          <View className="flex-row space-x-10 px-4 py-8 bg-white my-1 items-center ">
+            <Image
+              source={require('../Assets/images/MTN.jpg')}
+              className=" bg-gray-300 h-10 w-10 "
+            />
+            <Text className=" text-lg">MTN Mobile Money</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity className="">
+          <View className="flex-row space-x-10 px-4 py-8 bg-white my-1 items-center ">
+            <Image
+              source={require('../Assets/images/MTN.jpg')}
+              className=" bg-gray-300 h-10 w-10 "
+            />
+            <Text className=" text-lg">MTN Mobile Money</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity className="">
+          <View className="flex-row space-x-10 px-4 py-8 bg-white my-1 items-center ">
+            <Image
+              source={require('../Assets/images/MTN.jpg')}
+              className=" bg-gray-300 h-10 w-10 "
+            />
+            <Text className=" text-lg">MTN Mobile Money</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity className="">
+          <View className="flex-row space-x-10 px-4 py-8 bg-white my-1 items-center ">
+            <Image
+              source={require('../Assets/images/MTN.jpg')}
+              className=" bg-gray-300 h-10 w-10 "
+            />
+            <Text className=" text-lg">MTN Mobile Money</Text>
+          </View>
+        </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
   );
