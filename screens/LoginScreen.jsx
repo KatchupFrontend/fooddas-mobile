62c7daf7117678bbc61079574ad5b39d8ded2528
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-01-22 23:45:30 +0000

    login functioinality

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,10 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+} from 'firebase/auth';
+import React, {useState} from 'react';
 import {useLayoutEffect} from 'react';
 import {
   Image,
@@ -11,9 +16,37 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {auth} from '../firebase/firebase';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigation = useNavigation();
+
+  const handleLogin = e => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+ // const provider = new GoogleAuthProvider();
+
+
+  handleGoogle = async () => {
+  await  signInWithRedirect(auth, provider)
+  }
+  
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
@@ -48,17 +81,22 @@ const LoginScreen = () => {
           placeholder="Enter your email"
           keyboardType="default"
           className=" p-2 border-2 border-gray-300 rounded-lg w-[360px] mt-10  text-lg"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          textContentType="emailAddress"
         />
         <TextInput
           placeholder="Enter your password"
           keyboardType="default"
           className=" p-2 border-2 border-gray-300 rounded-lg w-[360px] mt-4 text-lg"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          textContentType="password"
         />
 
         <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('SignUpScreen');
-          }}
+          onPress={handleLogin
+          }
           className="bg-red-500 rounded-lg w-[360px] mt-10 py-3">
           <Text className="text-center text-white font-bold text-xl">
             Login
@@ -68,9 +106,7 @@ const LoginScreen = () => {
         <Text className="py-4 font-bold  text-lg">Or continue with</Text>
 
         <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('SignUpScreen');
-          }}
+          onPress={handleGoogle}
           className="bg-white border-gray-500  border w-[360px] p-2 rounded-lg flex-row justify-center">
           <View className=" flex-row items-center">
             <Image
@@ -83,11 +119,11 @@ const LoginScreen = () => {
 
         <Text className="my-2 text-lg">
           Don't have an account?
-          <Text
-            onPress={() => {
+          <Text onPress={
+            () => {
               navigation.navigate('SignUpScreen');
-            }}
-            className="text-red-500  text-lg">
+            }
+          } className="text-red-500  text-lg">
             Sign Up
           </Text>
         </Text>
