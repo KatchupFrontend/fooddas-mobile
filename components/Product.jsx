198b8c9afdfc4688475ee66d450d9df2be957741
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-02-03 23:58:01 +0000

    product card

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,1 +1,25 @@
-rnfce
+//react naitive
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+import React from 'react';
+
+
+
+const Product = ({product, navigation}) => {
+
+    return (
+        <TouchableOpacity
+        onPress={() => {
+            navigation.navigate('ProductDetails', {
+            product: product,
+            });
+        }}
+        className="flex-col items-center ">
+        <Image
+            source={{uri: product.image}}
+            style={{width: 100, height: 100}}
+        />
+        <Text className="text-lg text-black text-center">{product.title}</Text>
+        <Text className="text-lg text-black text-center">{product.price}</Text>
+        </TouchableOpacity>
+    );
+    }
